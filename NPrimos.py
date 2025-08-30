7a2b3bea55b638f183d8f52eed47c2234efec63f
Dario <dario.rodriguez@iteso.mx>
2025-08-29 20:58:38 -0600

    primer commit

---
+++ NPrimos.py
@@ -0,0 +1,33 @@
+import math
+
+
+def es_primo(num: int) -> bool:
+    if num <= 1:
+        return False
+    if num <= 3:
+        return True
+    if num % 2 == 0:
+        return False
+
+    raiz = int(math.sqrt(num))+1
+    for i in range(3, raiz, 2):
+        if num % i == 0:
+            return False
+    return True
+
+
+def primos(num: int) -> list[int]:
+    if num < 2:
+        return []
+
+    lista = [2]
+
+    for n in range(3, num + 1, 2):
+        if es_primo(n):
+            lista.append(n)
+    return lista
+
+
+print(primos(20))
+
+
